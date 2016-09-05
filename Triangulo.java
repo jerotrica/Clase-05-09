8bf76cb54146f5e27d849d5fa270ec8a58bccf5c
laboratorios <laboratorios@unlmlab.edu.ar>
2016-09-05 08:53:06 -0300

    Iniciando clase 05-09

---
+++ Triangulo.java
@@ -0,0 +1,21 @@
+package ar.edu.unlam.pb2;
+
+public class Triangulo {
+	private Double lado1Triangulo, lado2Triangulo, lado3Triangulo, baseTriangulo, alturaTriangulo;
+	
+	
+	public Double calcularPerimetro(){
+	Double perimetro;
+		perimetro=lado1Triangulo+lado2Triangulo+lado3Triangulo;
+	return perimetro;
+	}
+
+	public Double calcularArea(){
+	Double perimetro;
+		perimetro=lado1Triangulo+lado2Triangulo+lado3Triangulo;
+	return perimetro;
+	}
+	
+	
+	
+}
---
+++ src/ar/edu/unlam/pb2/Triangulo.java
@@ -0,0 +1,6 @@
+package ar.edu.unlam.pb2;
+
+public class Triangulo {
+	private Double lado1Triangulo, lado2Triangulo, lado3Triangulo, baseTriangulo, alturaTriangulo;
+	private Double areaTriangulo, perimetroTriangulo;
+}
