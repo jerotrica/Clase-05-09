bec43c8c340fc77ee8af360258f1c3e69cebf44e
laboratorios <laboratorios@unlmlab.edu.ar>
2016-09-05 08:58:04 -0300

    Update

--- Triangulo.java
+++ Triangulo.java
@@ -1,21 +1,19 @@
 package ar.edu.unlam.pb2;
 
 public class Triangulo {
-	private Double lado1Triangulo, lado2Triangulo, lado3Triangulo, baseTriangulo, alturaTriangulo;
-	
-	
-	public Double calcularPerimetro(){
-	Double perimetro;
-		perimetro=lado1Triangulo+lado2Triangulo+lado3Triangulo;
-	return perimetro;
+	private Double lado1Triangulo, lado2Triangulo, lado3Triangulo,
+			baseTriangulo, alturaTriangulo;
+
+	public Double calcularPerimetro() {
+		Double perimetro;
+		perimetro = lado1Triangulo + lado2Triangulo + lado3Triangulo;
+		return perimetro;
 	}
 
-	public Double calcularArea(){
-	Double perimetro;
-		perimetro=lado1Triangulo+lado2Triangulo+lado3Triangulo;
-	return perimetro;
+	public Double calcularArea() {
+		Double area;
+		area = (baseTriangulo * alturaTriangulo) / 2;
+		return area;
 	}
-	
-	
-	
+
 }
